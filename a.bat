e9278e1a928866d5451353be3ed79041be9d5d6f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-07-10 16:51:27 +0900

    master
    v-1.0
    
    Initial

---
+++ a.bat
@@ -0,0 +1,7 @@
+REM start %SystemRoot%\system32\cmd.exe
+start %SystemRoot%\system32\cmd.exe
+
+start C:\WORKS\PROGRAMS\FreeMind\Freemind.exe C:\WORKS\WORKSPACES_ANDROID\FM\NewsReader3_R\NewsReader3.mm
+gitk
+
+:end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsReader3::Application.config.secret_token = '88f824420d67208923467509fd1fd0a182af0664c23b95f7aadfbfac087a0226eea421c40fc599df0b6ce7cc057c07bf508330b92e3c358ec8e6aae6309593e2'
