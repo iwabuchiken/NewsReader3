520d1de944e1e524964739d7b538c995d99bcd3d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-07-10 16:38:55 +0900

    master
    v-1.0
    
    Initial

---
+++ a.bat
@@ -0,0 +1,7 @@
+REM start %SystemRoot%\system32\cmd.exe
+start %SystemRoot%\system32\cmd.exe
+
+start C:\WORKS\PROGRAMS\FreeMind\Freemind.exe C:\WORKS\WORKSPACES_APTANA\FM_APTANA\NewsReader3\NewsReader3.mm
+gitk
+
+:end
